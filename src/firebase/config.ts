76b4232793faf48e404c4eaf5cb5b9f6298b9602
bishal-pc <112154374+bishal-pc@users.noreply.github.com>
2026-03-07 05:43:45 +0530

    Remove Firebase configuration from config.ts
    Safety reasons

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,3 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAv58XVir_dDBVdccOOI8V0b-4zXyEqfaQ",
-  authDomain: "studio-7876391597-110db.firebaseapp.com",
-  projectId: "studio-7876391597-110db",
-  storageBucket: "studio-7876391597-110db.firebasestorage.app",
-  messagingSenderId: "692537841147",
-  appId: "1:692537841147:web:fd7335d8017679ce2ca75e"
-};
+
